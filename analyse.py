61c759dc876fff31cccd60c62649c08a2ad542fa
unruffled-nightingale <unruffled.nightingale@gmail.com>
2018-07-26 22:19:23 +0100

    Added class to construct usable DataFrame to analyse/track work log

---
+++ analyse.py
@@ -0,0 +1,91 @@
+import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+
+class Analyse(object):
+
+    def __init__(self, json_data):
+        self.df_raw = pd.DataFrame(json_data)
+        self.df = None
+        self.df_bar = None
+        self.df_line = None
+
+    def transform(self):
+        """
+        Transforms raw work_tracker_log data into a more useful DataFrame
+        with the following structure:
+        | TASK | STARTED | FINISHED | TIME_SPENT |
+        """
+
+        # Copy from our original DataFrame
+        self.df = self.df_raw.copy(deep=True)
+
+        # Remove the user_id col as it is not needed
+        del self.df['user_id']
+
+        # Rename TSTAMP column to STARTED
+        self.df.rename(columns={'tstamp': 'started'}, inplace=True)
+
+        # As each new task starts at the same time as the previous one finished
+        # we can sort by tstamp (or started) and shift by -1 to get the finish time
+        # for each task
+        self.df.sort_values(by='started')
+        self.df['finished'] = self.df.started.shift(-1)
+
+        # Last row will have a null as it has no finish date, so we remove it
+        self.df = self.df[self.df.finished.notnull()]
+
+        # Calculate the time spect on each task by taking the difference
+        # between the finish and the start
+        self.df['time_spent'] = (self.df.finished - self.df.started).astype('timedelta64[s]')
+
+    def set_bar_grouping(self):
+        """
+        Creates a new bar graph Dataframe from the transformed DataFrame. It shows
+        the total time spent on each task. It has the following structure:
+        | TASK | TIME_SPENT |
+        """
+
+        # Make sure we have ran transform
+        if not self.df:
+            self.transform()
+
+        # Copy from our transformed DataFrame
+        self.df_bar = self.df.copy(deep=True)
+
+        # Group by task and sum the time spent for each task
+        self.df_bar = self.df.groupby(['task'])[['time_spent']].sum().reset_index()
+        return self.df_bar
+
+    def set_line_grouping(self):
+        """
+        Creates a newline graph Dataframe from the transformed DataFrame. It shows
+        the accumulative time spent on each task over time. It has the following structure:
+        |||||
+        """
+        if not self.df:
+            self.transform()
+
+        # Copy from our transformed DataFrame
+        self.df_line = self.df.copy(deep=True)
+
+        # Calculate the accumulative time spent at the end of each task, for each task
+        self.df_line['total_time_spent_at_finish'] = self.df_line.groupby('task').time_spent.cumsum()
+
+        # Shift by 1 to get the accumulative time spent at the start of each task.
+        # This generates nulls for the first instance. We can set these nulls to 0 to indicate the
+        # start of time
+        self.df_line['total_time_spent_at_start'] = self.df_line.groupby(['task'])['total_time_spent_at_finish'].shift(1).fillna(0)
+
+        # We want to merge the start and the finish into one column, and the total_time_spent_at_start and
+        # total_time_at_finish into one column respectively.
+        # Doing this by splitting the DataFrames and appending them together.
+        # NOTE: There is almost definitely a better way to do this, but running out of time
+        df_start = self.df_line[['task', 'started', 'total_time_spent_at_start']]
+        df_start.rename(columns={'started': 'time', 'total_time_spent_at_start': 'total_time_spent'}, inplace=True)
+
+        df_finish = self.df_line[['task', 'finished', 'total_time_spent_at_finish']]
+        df_finish.rename(columns={'finished': 'time', 'total_time_spent_at_finish': 'total_time_spent'}, inplace=True)
+        self.df_line = df_start.append(df_finish, ignore_index=True)#.sort_values('time')
+
+
---
+++ test_analyse.py
@@ -0,0 +1,103 @@
+import unittest
+import datetime
+from pandas import DataFrame
+from pandas.util.testing import assert_frame_equal
+from analyse import Analyse
+
+
+class TestAnalyse(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+
+        # Construct our original data set
+        cls.data = [{'user_id': 'test user', 'task': '1', 'tstamp': datetime.datetime(2018, 7, 26, 20, 00, 00, 000000)},
+                {'user_id': 'test user', 'task': '2', 'tstamp': datetime.datetime(2018, 7, 26, 20, 10, 00, 000000)},
+                {'user_id': 'test user', 'task': '1', 'tstamp': datetime.datetime(2018, 7, 26, 20, 15, 00, 000000)},
+                {'user_id': 'test user', 'task': '2', 'tstamp': datetime.datetime(2018, 7, 26, 20, 20, 00, 000000)},
+                {'user_id': 'test user', 'task': '1', 'tstamp': datetime.datetime(2018, 7, 26, 20, 40, 00, 000000)}
+                ]
+
+        # Initalise our instance of Analyse
+        cls.analyse = Analyse(cls.data)
+
+        # Construct some DataFrames to use later in our unit test package
+
+        # For comparing analyse.df_raw
+        cls.raw_df = DataFrame(cls.data)
+
+        # For comparing analyse.df_transform
+        cls.transform_df = DataFrame([
+            {'task': '1', 'started': datetime.datetime(2018, 7, 26, 20, 00, 00, 000000),
+             'finished': datetime.datetime(2018, 7, 26, 20, 10, 00, 000000), 'time_spent': 600.00},
+            {'task': '2', 'started': datetime.datetime(2018, 7, 26, 20, 10, 00, 000000),
+             'finished': datetime.datetime(2018, 7, 26, 20, 15, 00, 000000), 'time_spent': 300.00},
+            {'task': '1', 'started': datetime.datetime(2018, 7, 26, 20, 15, 00, 000000),
+             'finished': datetime.datetime(2018, 7, 26, 20, 20, 00, 000000), 'time_spent': 300.00},
+            {'task': '2', 'started': datetime.datetime(2018, 7, 26, 20, 20, 00, 000000),
+             'finished': datetime.datetime(2018, 7, 26, 20, 40, 00, 000000), 'time_spent': 1200.00}
+        ])
+        cls.transform_df = cls.transform_df[['task', 'started', 'finished', 'time_spent']]
+
+        # For comparing analyse.df_bar
+        cls.bar_df = DataFrame([
+            {'task': '1', 'time_spent': 900.00},
+            {'task': '2', 'time_spent': 1500.00}
+        ])
+        cls.bar_df = cls.bar_df[['task', 'time_spent']]
+
+        # For comparing analyse.df_line
+        cls.line_df = DataFrame([
+            {'task': '1', 'time': datetime.datetime(2018, 7, 26, 20, 00, 00, 000000), 'total_time_spent': 0.00},
+            {'task': '2', 'time': datetime.datetime(2018, 7, 26, 20, 10, 00, 000000), 'total_time_spent': 0.00},
+            {'task': '1', 'time': datetime.datetime(2018, 7, 26, 20, 15, 00, 000000), 'total_time_spent': 600.00},
+            {'task': '2', 'time': datetime.datetime(2018, 7, 26, 20, 20, 00, 000000), 'total_time_spent': 300.00},
+            {'task': '1', 'time': datetime.datetime(2018, 7, 26, 20, 10, 00, 000000), 'total_time_spent': 600.00},
+            {'task': '2', 'time': datetime.datetime(2018, 7, 26, 20, 15, 00, 000000), 'total_time_spent': 300.00},
+            {'task': '1', 'time': datetime.datetime(2018, 7, 26, 20, 20, 00, 000000), 'total_time_spent': 900.00},
+            {'task': '2', 'time': datetime.datetime(2018, 7, 26, 20, 40, 00, 000000), 'total_time_spent': 1500.00},
+
+        ])
+        cls.line_df = cls.line_df[['task', 'time', 'total_time_spent']]
+
+    def test_transform(self):
+        """ Testing the transform_raw functionality """
+
+        # Run transform_raw to get cleaned usable DataFrame
+        self.analyse.transform()
+
+        # Check that the DataFrames are the same
+        assert_frame_equal(self.analyse.df, self.transform_df)
+
+        # Check that the raw DataFrame is still in its correct state
+        assert_frame_equal(self.analyse.df_raw, self.raw_df)
+
+    def test_set_bar_grouping(self):
+        """ Testing the bar graph grouping """
+
+        # Run get_bar_grouping
+        self.analyse.set_bar_grouping()
+
+        # Check that the DataFrames are the same
+        assert_frame_equal(self.analyse.df_bar, self.bar_df)
+
+        # Check that the raw DataFrame is still in its correct state
+        assert_frame_equal(self.analyse.df, self.transform_df)
+        assert_frame_equal(self.analyse.df_raw, self.raw_df)
+
+    def test_set_line_grouping(self):
+        """ Testing the bar graph grouping """
+
+        # Run set_line_grouping
+        self.analyse.set_line_grouping()
+
+        # Check that the DataFrames are the same
+        # This fails as self.line_df.index is RangeIndex
+        assert_frame_equal(self.analyse.df_line, self.line_df)
+
+        # Check that the raw DataFrame is still in its correct state
+        assert_frame_equal(self.analyse.df, self.transform_df)
+        assert_frame_equal(self.analyse.df_raw, self.raw_df)
+
+
+
